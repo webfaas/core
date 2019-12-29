91f6b90c5d810aef7b29862d1b0003fcb8367580
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2019-12-29 12:53:50 -0300

    fix: build

--- src/lib/ClientHTTP/ClientHTTPConfig.ts
+++ src/lib/ClientHTTP/ClientHTTPConfig.ts
@@ -1,3 +1,5 @@
+import { KeyObject, PxfObject } from "tls";
+
 export class ClientHTTPConfig  {
     timeout: number = 100000
     rejectUnauthorized: boolean = true
@@ -5,11 +7,11 @@ export class ClientHTTPConfig  {
     maxSockets: number = 2
 
     // Necessary only if the server requires client certificate authentication.
-    key?: string | Buffer | Array<Buffer | Object>;
+    key?: string | Buffer | Array<Buffer | KeyObject>;
     cert?: string | Buffer | Array<string | Buffer>;
 
     //pfx is an alternative to providing key and cert
-    pfx?: string | Buffer | Array<string | Buffer | Object>
+    pfx?: string | Buffer | Array<string | Buffer | PxfObject>;
 
     // Necessary only if the server uses a self-signed certificate.
     ca?: string | Buffer | Array<string | Buffer>;
